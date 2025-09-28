182ab265e6c88c5a9513074710c53c00c44c1960
greemaltend4 <dengestabiy@hotmail.com>
2025-09-28 13:38:29 +0000

    Add a simple logging middleware for request logging.

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Logging middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Database connected'))
   .catch(err => console.error('Error connecting to the database', err));
